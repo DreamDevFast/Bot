dbd7178bdd233b96455b7d687485a0c8265d0e4d
DreamDevFast <dream.dev1215@gmail.com>
2023-05-20 23:18:52 +0000

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import styles from "@/styles/HomePage.module.css";
 
 const HomePage: NextPage = () => {
   const [botStatus, setBotStatus] = useState("unknown");
@@ -17,11 +18,17 @@ const HomePage: NextPage = () => {
   };
 
   return (
-    <div>
-      <h1>Discord bot control</h1>
-      <button onClick={startBot}>Start Bot</button>
-      <button onClick={stopBot}>Stop Bot</button>
-      <p>Bot status: {botStatus}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Discord bot control</h1>
+      <div className={styles.buttonWrapper}>
+        <button className={`${styles.button} ${styles.startButton}`} onClick={startBot}>
+          Start Bot
+        </button>
+        <button className={`${styles.button} ${styles.stopButton}`} onClick={stopBot}>
+          Stop Bot
+        </button>
+      </div>
+      <p className={styles.status}>Bot status: {botStatus}</p>
     </div>
   );
 };
