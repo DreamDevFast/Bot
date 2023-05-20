a91b20359f3e9b6ed5d9dbe83713abac465cb1e9
DreamDevFast <dream.dev1215@gmail.com>
2023-05-20 20:03:03 +0000

    update bot logic

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,71 +1,84 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Client, GatewayIntentBits } from 'discord.js'
-import createOpenAI, { AIChatMessage } from '@/openai/config'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Client, GatewayIntentBits } from "discord.js";
+import createOpenAI, { AIChatMessage } from "@/openai/config";
+import { isBot } from "next/dist/server/web/spec-extension/user-agent";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
-})
-const openAI = createOpenAI()
+});
+const openAI = createOpenAI();
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`)
-})
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
 
-client.on('messageCreate', async (message) => {
-  if (message.author.bot) return
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
 
-  if (message.content.startsWith(`<@!${client.user?.id}>`)) {
-    message.channel.sendTyping()
+  const isMentioned = message.content.startsWith(`<@${client.user?.id}>`);
+  const isInThread = message.channel.isThread();
+  const isBotThread =
+    isInThread && message.channel.name.startsWith("ChatGPT Thread with");
 
-    const question = message.content
-      .replace(`<@!${client.user?.id}>`, '')
-      .trim()
+  if (isMentioned || isBotThread) {
+    message.channel.sendTyping();
 
-    let completion
-    let attempts = 0
+    const text = isMentioned ? message.content.replace(`<@${client.user?.id}>`, '').trim() : message.content;
 
-    while (!completion) {
-      try {
-        const prompt: AIChatMessage[] = [
-          { role: 'user', content: `Please answer this question: ${question}` },
-        ]
+    const messages: AIChatMessage[] = [
+      { role: 'user', content: text },
+    ];
 
-        completion = await openAI.createChatCompletion({
-          model: 'gpt-4',
-          messages: prompt,
-          max_tokens: 1500,
-          temperature: 0.0,
-          top_p: 1.0,
-          frequency_penalty: 0.0,
-          presence_penalty: 0.0,
-        })
-
-        const answer = (
-          completion.data.choices[0].message?.content ?? ''
-        ).trim()
-        const thread = await message.startThread({
-          name: 'ChatGPT Thread',
-          autoArchiveDuration: 60,
-        })
-        thread.send(answer)
-      } catch (error) {
-        console.error(error)
-        message.reply('Sorry, I could not process your request.')
-        attempts += 1
-        if (attempts > 6) {
-          throw error
+    if (isBotThread) {
+      const fetchedMessages = await message.channel.messages.fetch({ limit: 10 });
+      fetchedMessages.forEach(fetchedMessage => {
+        if (fetchedMessage.author.bot) {
+          messages.push({ role: 'assistant', content: fetchedMessage.content });
         } else {
-          await sleep(500)
+          messages.push({ role: 'user', content: fetchedMessage.content });
         }
+      });
+    }
+
+    try {
+      const prompt: AIChatMessage[] = [
+        { role: "user", content: `Please answer this question: ${text}` },
+      ];
+
+      const completion = await openAI.createChatCompletion({
+        model: "gpt-4",
+        messages: messages,
+        max_tokens: 1500,
+        temperature: 0.0,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      });
+
+      const answer = (completion.data.choices[0].message?.content ?? "").trim();
+
+      console.log("answer = ", answer);
+
+      if (isMentioned) {
+        const thread = await message.startThread({
+          name: "ChatGPT Thread with " + message.author.username,
+          autoArchiveDuration: 60,
+        });
+        thread.send(answer);
+      } else {
+        message.channel.send(answer);
       }
+    } catch (error) {
+      console.error(error);
+      message.reply("Sorry, I could not process your request.");
     }
   }
-})
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN)
+client.login(process.env.DISCORD_BOT_TOKEN);
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json('Bot is running!')
+  res.status(200).json("Bot is running!");
 }
