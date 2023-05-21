9fd5f2a85bb7e4ec64f363d90ed69001e7a39a19
DreamDevFast <dream.dev1215@gmail.com>
2023-05-21 01:22:35 +0000

    extend typing indicator duration

--- lib/executeCommand.ts
+++ lib/executeCommand.ts
@@ -1,8 +1,33 @@
 import createOpenAI, { AIChatMessage } from "@/openai/config";
 import { sleep } from "@/utils";
-import { Message } from "discord.js";
+import { Message, TextBasedChannel, TextChannel, ThreadChannel } from "discord.js";
 
 const openAI = createOpenAI();
+
+interface PromiseWrapper {
+  promise: Promise<void>;
+  isFulfilled: boolean;
+}
+
+const sendTypingWithDelay = async (
+  channel: TextBasedChannel,
+  delayMs: number
+) => {
+  channel.sendTyping();
+
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, delayMs);
+  });
+};
+
+const sendExtendedTyping = async (
+  channel: TextBasedChannel,
+  promiseWrapper: PromiseWrapper
+) => {
+  while (!promiseWrapper.isFulfilled) {
+    await sendTypingWithDelay(channel, 8000); // Duration should be less than 10 seconds
+  }
+};
 
 const executeCommand = async (
   message: Message,
@@ -45,41 +70,51 @@ const executeCommand = async (
     let completion;
     let attempts = 0;
 
-    while (!completion) {
-      try {
-        completion = await openAI.createChatCompletion({
-          model: "gpt-4",
-          messages: messages,
-          max_tokens: 1500,
-          temperature: 0.0,
-          top_p: 1.0,
-          frequency_penalty: 0.0,
-          presence_penalty: 0.0,
-        });
+    const promiseWrapper: PromiseWrapper = {
+      promise: Promise.resolve(),
+      isFulfilled: false,
+    };
 
-        const answer = (
-          completion.data.choices[0].message?.content ?? ""
-        ).trim();
+    promiseWrapper.promise = (async () => {
+      while (!completion) {
+        sendExtendedTyping(message.channel, promiseWrapper);
 
-        if (isMentioned && !isBotThread) {
-          const thread = await message.startThread({
-            name: "ChatGPT Thread with " + message.author.username,
-            autoArchiveDuration: 60,
+        try {
+          completion = await openAI.createChatCompletion({
+            model: "gpt-4",
+            messages: messages,
+            max_tokens: 1500,
+            temperature: 0.0,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
           });
-          thread.send(answer);
-        } else {
-          message.channel.send(answer);
-        }
-      } catch (error) {
-        console.error(error);
-        attempts += 1;
-        if (attempts > 6) {
-          message.reply("Sorry, I could not process your request.");
-        } else {
-          await sleep(500);
+
+          const answer = (
+            completion.data.choices[0].message?.content ?? ""
+          ).trim();
+
+          if (isMentioned && !isBotThread) {
+            const thread = await message.startThread({
+              name: "ChatGPT Thread with " + message.author.username,
+              autoArchiveDuration: 60,
+            });
+            thread.send(answer);
+          } else {
+            message.channel.send(answer);
+          }
+          promiseWrapper.isFulfilled = true;
+        } catch (error) {
+          console.error(error);
+          attempts += 1;
+          if (attempts > 6) {
+            message.reply("Sorry, I could not process your request.");
+          } else {
+            await sleep(500);
+          }
         }
       }
-    }
+    })();
   }
 };
 
